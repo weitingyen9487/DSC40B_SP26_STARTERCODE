c919cf05402fbc1ec390621bd6619a5104b7d779
weitingyen9487 <wyen@ucsd.edu>
2026-04-22 15:49:17 -0700

    Create histogram.py

---
+++ histogram.py
@@ -0,0 +1,26 @@
+def histogram(points, bins):
+    """Efficiently computes a histogram.
+
+    Assumes that both `points` and `bins` are sorted in ascending order to
+    avoid looping through all bins for each point.
+
+    """
+    n = len(points)
+    result = []
+    
+    i = 0  # pointer for points
+    
+    for (a, b) in bins:
+        count = 0
+        
+        # count points in current bin [a, b)
+        while i < n and points[i] < b:
+            if points[i] >= a:
+                count += 1
+            i += 1
+        
+        width = b - a
+        density = count / (n * width)
+        result.append(density)
+    
+    return result
