a0a40f5fb7545834c5c1a0a237c206fa0f90bba5
weitingyen9487 <wyen@ucsd.edu>
2026-04-22 15:38:04 -0700

    Update swap_sum.py

--- hw3/swap_sum.py
+++ hw3/swap_sum.py
@@ -6,6 +6,28 @@ def swap_sum(A, B):
     contain integers.
 
     """
-    # TODO: Implement the swap_sum function
+    sumA = sum(A)
+    sumB = sum(B)
+    
+    diff = sumA + 10 - sumB
+    
+    # must be even to divide by 2
+    if diff % 2 != 0:
+        return None
+    
+    target = diff // 2  # we want a - b = target
+    
+    i = 0
+    j = 0
+    
+    while i < len(A) and j < len(B):
+        current = A[i] - B[j]
+        
+        if current == target:
+            return (i, j)
+        elif current < target:
+            i += 1
+        else:
+            j += 1
     
     return None
